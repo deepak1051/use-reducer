4f42a568857a20faed9767cad6f764cb8b0edf6c
deepak1051 <deepakpanwar1051@gmail.com>
2023-01-25 23:30:58 +0530

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
+import produce from 'immer';
 import './App.css';
 
+const INCREMENT_COUNT = 'increment-count';
+const CHANGE_VALUE = 'change-value-to-add';
+const DECREMENT = 'decrement-count';
+const SUBMIT_FORM = 'submit-form';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case CHANGE_VALUE:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case SUBMIT_FORM:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+      };
+
+    default:
+      return state;
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(produce(reducer), {
+    count: 10,
+    valueToAdd: 0,
+  });
+
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  };
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: SUBMIT_FORM,
+    });
+  };
+  const handleChange = (e) => {
+    // setValueToAdd(parseInt(e.target.value) || 0)
+    const value = parseInt(e.target.value) || 0;
+    dispatch({
+      type: CHANGE_VALUE,
+      payload: value,
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count is {state.count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={state.valueToAdd || ''}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 }
